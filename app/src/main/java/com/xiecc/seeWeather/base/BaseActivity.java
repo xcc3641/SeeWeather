5147cee08ad98e4c4d2c5a8ceb7f9deaabcbaa9d
Hugo <446022570@qq.com>
2017-05-29 20:50:23 +0800

    update watcher & remove useless file

--- app/src/main/java/com/xiecc/seeWeather/base/BaseActivity.java
+++ app/src/main/java/com/xiecc/seeWeather/base/BaseActivity.java
@@ -1,43 +1,11 @@
 package com.xiecc.seeWeather.base;
 
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
-import android.view.WindowManager;
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
 public class BaseActivity extends RxAppCompatActivity {
     private static String TAG = BaseActivity.class.getSimpleName();
-
-    /**
-     * 设置状态栏颜色
-     * 也就是所谓沉浸式状态栏
-     */
-    @Deprecated
-    public void setStatusBarColor(int color) {
-        /**
-         * Android4.4以上  但是抽屉有点冲突，目前就重写一个方法暂时解决4.4的问题
-         */
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            SystemBarTintManager tintManager = new SystemBarTintManager(this);
-            tintManager.setStatusBarTintEnabled(true);
-            tintManager.setStatusBarTintResource(color);
-        }
-    }
-
-    public void setStatusBarColorForKitkat(int color) {
-        /**
-         * Android4.4
-         */
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
-            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            SystemBarTintManager tintManager = new SystemBarTintManager(this);
-            tintManager.setStatusBarTintEnabled(true);
-            tintManager.setStatusBarTintResource(color);
-        }
-    }
 
     @Override
     protected void onStop() {
@@ -65,14 +33,6 @@ public class BaseActivity extends RxAppCompatActivity {
             AppCompatDelegate.MODE_NIGHT_YES);
         // 调用 recreate() 使设置生效
         activity.recreate();
-    }
-
-    public void setTheme(boolean isNights, AppCompatActivity activity) {
-        if (isNights) {
-            setNightTheme(activity);
-        } else {
-            setDayTheme(activity);
-        }
     }
 
     public void setTheme(AppCompatActivity activity) {
